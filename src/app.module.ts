4919d3802a22c47943a93cf8dda398083470b28b
Aknologia <aknologia.officiel@gmail.com>
2022-02-25 18:07:58 +0100

    add Throttler for Rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UriProvider } from './db/db-uri.provider';
@@ -9,16 +11,30 @@ import { UsersModule } from './modules/users.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        ttl: config.get<number>('THROTTLE_TTL'),
+        limit: config.get<number>('THROTTLE_LIMIT'),
+      }),
+    }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        uri: new UriProvider(configService).getMain(),
-      }),
       inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: new UriProvider(config).getMain(),
+      }),
     }),
     UsersModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
