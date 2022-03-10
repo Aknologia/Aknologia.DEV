a703c61565157d46bef8eaad291351d3d8db4a55
Aknologia <aknologia.officiel@gmail.com>
2022-03-10 17:48:30 +0100

    try adding 4 columns to dependency table

--- .github/runner.py
+++ .github/runner.py
@@ -16,13 +16,30 @@ with open(os.path.join(os.getcwd(), 'package.json'), 'r') as packageFile:
     packages = json.loads(packageFile.read())
     with open(os.path.join(os.getcwd(), 'README.md'), 'r') as readmeFile:
         data = readmeFile.read(); ref = data
-        lines = []
-        for pkg in list(packages['dependencies'].keys()):
+
+        # Normalize Packages as Table Rows
+        lines = [[], []]
+        depKeys = packages['dependencies'].keys()
+        firstHalf = depKeys[:round(len(depKeys)/2)]
+        latterHalf = depKeys[round(len(depKeys)/2):]
+        for pkg in list(firstHalf):
             ver = packages['dependencies'][pkg]
-            lines.append(f'| [{pkg}](https://www.npmjs.com/package/{pkg}) | `{ver}` |')
+            lines[0].append(f'| [{pkg}](https://www.npmjs.com/package/{pkg}) | `{ver}` |')
+        for pkg in list(latterHalf):
+            ver = packages['dependencies'][pkg]
+            lines[0].append(f' [{pkg}](https://www.npmjs.com/package/{pkg}) | `{ver}` |')
+        flines = []
+        for l in lines[0]: flines.append(l)
+        for i in range(len(lines[1])):
+            if(len(lines[1]) <= i): flines.append('|'+lines[1][i] + ' | |')
+            else: flines.append(lines[1][i])
+        
+        # Replace Old Data
         start, end = data.index(plStart) + len(plStart), data.index(plEnd)-1
         oldData = data[start:end]
-        data = data.replace(oldData, "\n| Package | Version |\n| ------- | ------- |\n"+"\n".join(lines))
+        data = data.replace(oldData, "\n| Package | Version | | |\n| ------- | ------- | ------- | ------- |\n"+"\n".join(lines))
+        
+        # Write new Data if changed
         if data != ref:
             with open(os.path.join(os.getcwd(), 'README.md'), 'w') as writable:
                 writable.write(data)
